832d122ed1b86dbf48b1b4d608bc54e0c8174f4f
VinciusBarrosFiap <rm97824@fiap.com.br>
2023-03-13 13:49:24 -0300

    exercicio equacao segundo grau

--- pg015.py
+++ pg015.py
@@ -1,7 +1,18 @@
+import math
 a = float(input("Digite o valor de A:"))
 b = float(input("Digite o valor de B:"))
 c = float(input("Digite o valor de C:"))
 
 delta = (b**2) - 4 * a * c
 
-x = -b
+raiz = delta**0.5
+
+x_pos = (b * (-1) + raiz) / (2 * a)
+
+x_neg = (b * (-1) - raiz) / (2 * a)
+
+if delta < 0:
+    print("As raizes não são reais")
+else:
+
+    print(f"As raizes são x1: {x_pos} x2: {x_neg}")
