a7e5b8dcb4362517a6e513f9a3ebc8657e11078d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-13 11:18:49 -0300

    exercicios

---
+++ pg009.py
@@ -0,0 +1,11 @@
+#Entrada das notas
+print("Digite suas 4 notas: ")
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 =  float(input())
+
+media = (nota1 + nota2 + nota3 + nota4) / 4 #calcula a média da pessoa
+
+print(f"Suas notas são: Nota 1: {nota1}, Nota 2: {nota2}, Nota 3: {nota3}, Nota 4: {nota4} ")
+print("Sua média é: ", media)
