b6ef4756348310015b45da96bad2437cfb9343e5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 09:05:01 -0300

    comentario

--- verifica-idade.py
+++ verifica-idade.py
@@ -1,6 +1,9 @@
 idade = int(input("Digite sua idade: ")) #entrada de dados (idade)
-if idade > 18:
+
+if idade >= 18:
     print("Você é obrigado a votar")
 
 else:
     print("Você não é obrigado a votar")
+
+print("Fim do programa")
