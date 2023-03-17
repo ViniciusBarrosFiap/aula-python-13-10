3a5624148444bb258f6515ac9151127f49ee15b5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-17 08:51:20 -0300

    arquivos renomeados

---
+++ idade.py
@@ -0,0 +1,4 @@
+ano_atual = int(input("Digite o ano atual: "))
+ano_nascimento = int(input("Digite o ano que você nasceu: "))
+idade = ano_atual - ano_nascimento
+print("Você tem", idade, "anos de idade")
