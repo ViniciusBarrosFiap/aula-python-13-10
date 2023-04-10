423f364d72466ec083265c19526b0f1497883c76
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-04-10 12:54:18 -0300

    Exercicio fibonacci resolvido

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,16 @@
 num = int(input("Digite um numero maior ou igual a 2: "))
+ultimo = 1
+penultimo = 1
 if num < 2:
     print("Numero invalido")
 
 else:
+    print(penultimo)
+    print(ultimo)
     for i in range(1, num, 1):
-        fibo = 1 + 1
-        fibo1 = fibo - 1
+        resultado = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = resultado
+        print(resultado)
+
+        
