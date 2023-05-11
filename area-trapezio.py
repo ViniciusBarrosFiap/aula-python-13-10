3e6fae9bfe22560316571bb163ff08a2f1428d17
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-05-11 19:22:15 -0300

    .

--- area-trapezio.py
+++ area-trapezio.py
@@ -1,7 +1,7 @@
-base_maior = float(input("Digite a base maior do trapézio: "))
-base_menor = float(input("Digite a base menor do trapézio: "))
-altura = float(input("Digite a altura do trapézio: "))
+base_maior_trap = float(input("Digite a base maior do trapézio: "))
+base_menor_trap = float(input("Digite a base menor do trapézio: "))
+altura_trap = float(input("Digite a altura do trapézio: "))
 
-area = ((base_maior * base_menor) * altura) / 2
+area_trap = ((base_maior + base_menor) * altura) / 2
 
 print("A area do trapézio equivale a:", area)
