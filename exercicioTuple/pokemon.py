6d03093c4e6fcc35e28917c955a0241eb89f5ba7
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-08-13 17:07:19 -0300

    <exercicio>Terminado

--- exercicioTuple/pokemon.py
+++ exercicioTuple/pokemon.py
@@ -1,13 +1,17 @@
-pokemons = ("Charmeleon", "FOGO", "Charmander", "FOGO", "Charizard", "FOGO", "Squirtle", "Agua", "Blastoise", "Agua", "Metapod", "Inseto", "Pidgeoto", "Normal","Pidgey", "Normal", "Rattata", "Normal", "Raticate", "Normal")
+def verificaPokemon(pokemons, tipo):
+    contador = 0
+    pokemons_tipo = []
+    for i in range(0, len(pokemons), 2):
+        if pokemons[i + 1] == tipo:
+            contador += 1
+            pokemons_tipo.append(pokemons[i])
+    if contador == 0:
+        print(f"não existe pokemons do tipo: {tipo}")
+    else:
+        pokemons_formatado = ", ".join(pokemons_tipo)
+        print(f"A tupla possui {contador} do tipo {tipo} e eles são: {pokemons_formatado}")
 
+pokemons = ("Charmeleon", "FOGO", "Charmander", "FOGO", "Charizard", "FOGO", "Squirtle", "AGUA", "Blastoise", "AGUA", "Metapod", "INSETO", "Pidgeoto", "NORMAL","Pidgey", "NORMAL", "Rattata", "NORMAL", "Raticate","NORMAL")
 pokemonUser = input("Digite um tipo de pokemon: ").upper()
 
-if pokemonUser == "FOGO":
-    print(pokemons.count("FOGO"))
-    for i in pokemons:
-        if i == "FOGO":
-            i += 1
-            print(i-1)
-    
-        
-
+verificaPokemon(pokemons, pokemonUser)
