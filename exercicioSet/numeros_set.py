123e01288fb985cc4d935ad2494bab963838a860
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-08-14 11:04:29 -0300

    <Exercicio>Numeros set feito

---
+++ exercicioSet/numeros_set.py
@@ -0,0 +1,34 @@
+numeros = {0}
+tipo_numero_usuario = input('''Escolha um tipo de número para digitar\n
+"R" para numeros REAIS
+"I" para números INTEIROS\n''').upper()
+
+if tipo_numero_usuario == "R":
+    tipo_numero_usuario = "REAL"
+
+elif tipo_numero_usuario == "I":
+    tipo_numero_usuario = "INTEIRO"
+
+else:
+    print("Escolha um tipo de número válido")
+
+maior_numero = 0
+numeros.remove(0)
+while len(numeros) < 10:
+    try:
+        if tipo_numero_usuario == "REAL":
+            numeros_usuario = float(input("DIgite um número REAL: "))
+            numeros.add(numeros_usuario)
+            for i in numeros:
+               if i >= maior_numero:
+                   maior_numero = i
+
+
+        elif tipo_numero_usuario == "I":
+            numeros_usuario = int(input("Digite um numero INTEIRO: "))
+            numeros.add(numeros_usuario)
+    except ValueError:
+        print("Digite um valor válido")
+
+print(f"Você escolheu digitar números do tipo: {tipo_numero_usuario}")
+print(f"O maior número digitado foi: {maior_numero}")
