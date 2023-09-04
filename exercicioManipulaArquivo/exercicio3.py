60ddf2811c4048927c5b0af524284c6bc63d0c1f
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-04 15:30:14 -0300

    exercicio 3 pronto

--- exercicioManipulaArquivo/exercicio3.py
+++ exercicioManipulaArquivo/exercicio3.py
@@ -1,15 +1,21 @@
+numeroUser = 1
+numeros = set()
 try:
-    numeroUser = 1
-    numeros = {}
     while numeroUser != 0:
         numeroUser = int(input("Digite números inteiros: "))
         if numeroUser != 0:
             numeros.add(numeroUser)
-        f = open("exercicioManipulaArquivo/numeros.txt", "a", encoding="utf-8")
-        f.write(numeros)
-        f.close()
-        f = open("exercicioManipulaArquivo/numeros.txt", "r", encoding="utf-8")
-        print("Numeros adicionados:\n"+ f.read())
-except:
-    print("Arquivo não existe")
-    
+    numeros_pares = set()
+
+    for i in numeros:
+        if i % 2 == 0:
+            numeros_pares.add(str(i))
+
+    f = open("exercicioManipulaArquivo/numeros.txt", "a", encoding="utf-8")
+    for i in numeros_pares:
+        f.write(str(i) + " ")
+    f.close()
+    f = open("exercicioManipulaArquivo/numeros.txt", "r", encoding="utf-8")
+    print("Numeros adicionados:\n"+ f.read())
+except FileNotFoundError:
+    print('O arquivo não existe')
