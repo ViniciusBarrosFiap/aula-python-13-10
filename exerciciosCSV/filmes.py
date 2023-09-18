e514a4ab8788e02868ec72420eea95cc40d66f4a
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-18 13:50:29 -0300

    exercicios corrigidos

--- exerciciosCSV/filmes.py
+++ exerciciosCSV/filmes.py
@@ -1,3 +1,4 @@
+#Rm97824 - Vinicius Oliveira de Barros
 import csv
 continuar = "sim"
 novos_filmes = []
@@ -8,13 +9,13 @@ while continuar == "sim":
     genero = input()
     print("Digite a duração do filme: ")
     duracao = input()
-    print("Digite a censura do filme: ")
+    print("Digite a sensura do filme: ")
     sensura = input()
     print("Digite o nome do diretor do filme: ")
     diretor = input()
     novos_filmes.append([titulo, genero, duracao + "minutos", sensura + "anos", diretor])
-    with open("d:/filmes.csv", "a", encoding="utf-8", newline="") as arquivo:
-        adiciona_filmes = csv.writer(arquivo)
-        for i in novos_filmes:
-            adiciona_filmes.writerow(i)
     continuar = input("Deseja cadastrar outro filme ?\n")
+with open("d:/filmes.csv", "a", encoding="utf-8", newline="") as arquivo:
+    adiciona_filmes = csv.writer(arquivo)
+    for i in novos_filmes:
+        adiciona_filmes.writerow(i)
--- exerciciosCSV/menu.py
+++ exerciciosCSV/menu.py
@@ -1,18 +1,20 @@
+#Rm97824 - Vinicius Oliveira de Barros
 import csv
 print("Escolha um filtro:\n 1- Titulo\n2- Diretor\n 3- Genero\n")
 opcao = int(input())
+if opcao == 1:
+    opcao_escolha = input("Digite o nome do titulo: ")
+elif opcao == 2:
+    opcao_escolha = input("Digite o nome do diretor: ")
+elif opcao == 3:
+    opcao_escolha = input("Digite o nome do diretor: ")
 
 with open("D:/filmes.csv", "r", encoding="utf-8") as f:
     filmes = csv.reader(f)
     next(filmes)
-    if opcao == 1:
-        opcao_escolha = input("Digite o nome do titulo: ")
-    elif opcao == 2:
-        opcao_escolha = input("Digite o nome do diretor: ")
-    elif opcao == 3:
-        opcao_escolha = input("Digite o nome do diretor: ")
-    for linha in filmes: #Ignora a primeira linha, pois é apenas os cabeçalhos das col
-        if opcao_escolha in filmes:
-            for i in linha:
-                print (i + "\t")
-    
+    for i in filmes:
+        for j in i:
+            if opcao_escolha in j:
+                print("LIVRO ENCONTRADO!")
+                for x in i:
+                    print(x)
